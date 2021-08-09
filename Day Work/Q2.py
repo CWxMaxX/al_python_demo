87f7b11d23e5806da65eda684997b6fd08d2eed9
CWxMaxX <s16538@sci.pdn.ac.lk>
2021-08-09 22:47:06 +0530

    Add Mark grding

---
+++ Day Work/Q2.py
@@ -0,0 +1,10 @@
+# Custom functions
+def printText(st):
+    print(st)
+
+
+i = 0
+
+while i < 10:
+    i = i + 1
+    printText(i)
---
+++ HW_Day1/Q8.py
@@ -0,0 +1,16 @@
+# Mark Grading Code
+mark = int(input("Enter mark of student : "))
+
+if mark > 75:
+    print("A")
+elif mark > 65:
+    print("B")
+elif mark > 55:
+    print("C")
+elif mark > 40:
+    print("D")
+else:
+    print("W")
+
+
+# CWx
