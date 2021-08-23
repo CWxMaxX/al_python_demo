cb7276a2414f82f153e82342c58bd8d7ca5c1111
CWxMaxX <s16538@sci.pdn.ac.lk>
2021-08-23 12:33:14 +0530

    Day 5

--- PP_Works/2019_12_e.py
+++ PP_Works/2019_12_e.py
@@ -1,10 +1,6 @@
 n = int(input("Input N : "))
 a = 0
 
-name = "Saman Perera"
-name2 = 'Saman Perera'
-name3 = """Saman Perera"""
-
 while n > 0:
     x = int(input("Input X : "))
     if x % 2 == 0:
