16c1b65f77a37c10b37578dc4473c60ee4c9994a
CWxMaxX <s16538@sci.pdn.ac.lk>
2021-08-28 17:31:48 +0530

    Tuple and dictionary

---
+++ DataStructures/Tuples/Tuple1.py
@@ -0,0 +1,9 @@
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5)
+tup3 = "a", "b", "c", "d"
+tup4 = ()
+tup5 = (25, 45, 65, 85, 45, 35)
+
+print("Max =", max(tup5))
+print("Min =", min(tup5))
+print("Length =", len(tup5))
